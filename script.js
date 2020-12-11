de8b39d1565fe80b6b396e1cdda6e05e798e01fd
Christian <christian.quinlan36@gmail.com>
2020-12-11 21:14:11 +0000

    adding starting variables for buttons, circles and progress bar

---
+++ script.js
@@ -0,0 +1,4 @@
+const nextButton = document.querySelector('#next');
+const prevButton = document.querySelector('#prev');
+const circles = document.querySelectorAll('.circle');
+const progressBar = document.querySelector('#progress');
