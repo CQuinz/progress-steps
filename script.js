c43fe5089088b46bc20d1c5d4b2289c4a2aff7e2
Christian <christian.quinlan36@gmail.com>
2020-12-14 13:08:31 +0000

    added event listeners and functions to check progress steps

--- script.js
+++ script.js
@@ -2,3 +2,35 @@ const nextButton = document.querySelector('#next');
 const prevButton = document.querySelector('#prev');
 const circles = document.querySelectorAll('.circle');
 const progressBar = document.querySelector('#progress');
+
+let progressPercent =33;
+
+let progressStep = 1;
+
+const checkProgressStep = ()=>{
+  if(progressStep >1){
+    prevButton.removeAttribute('disabled');
+  }else if(progressStep === 4){
+    nextButton.setAttribute('disabled');
+  }else if(progressStep === 1 ){
+    prevButton.setAttribute('disabled');
+  }
+}
+
+const goToNextStep = ()=>{
+  circles[progressStep].classList.add('active');
+  progressBar.style.width = `${progressPercent}%`;
+  progressStep++;
+  progressPercent += 33;
+}
+
+const goToPrevStep = ()=>{
+  circles[progressStep - 1].classList.remove('active');
+  progressStep--;
+  progressPercent -=33;
+  progressBar.style.width = `${progressPercent}%`;
+
+}
+
+nextButton.addEventListener('click', goToNextStep);
+prevButton.addEventListener('click', goToPrevStep);
