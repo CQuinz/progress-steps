e7f23fdc6c19778e31c6ea50aa404fa75f006e5f
Christian <christian.quinlan36@gmail.com>
2020-12-15 13:30:57 +0000

    working on logic conditions to display buttons

--- script.js
+++ script.js
@@ -7,31 +7,43 @@ let progressPercent =33;
 
 let progressStep = 1;
 
+
 const checkProgressStep = ()=>{
-  if(progressStep >1){
-    prevButton.removeAttribute('disabled');
-  }else if(progressStep === 4){
-    nextButton.setAttribute('disabled');
+  // if(progressStep >1){
+  //   prevButton.disabled = false;
+  // }else if(progressStep ===4){
+  //   nextButton.disabled = true;
+  // }else if(progressStep === 1 ){
+  //   prevButton.disabled = true;
+  // }
+
+  if(progressStep ===4){
+    nextButton.disabled = true;
+  }else if(progressStep >1){
+    prevButton.disabled = false;
   }else if(progressStep === 1 ){
-    prevButton.setAttribute('disabled');
+    prevButton.disabled = true;
   }
 }
 
 const goToNextStep = ()=>{
+  
   circles[progressStep].classList.add('active');
   progressBar.style.width = `${progressPercent}%`;
   progressStep++;
   progressPercent += 33;
   checkProgressStep();
+  console.log(progressStep);
 }
 
 const goToPrevStep = ()=>{
-  checkProgressStep();
+  
   circles[progressStep - 1].classList.remove('active');
   progressStep--;
   progressPercent -=33;
   progressBar.style.width = `${progressPercent}%`;
-
+  checkProgressStep();
+  console.log(progressStep);
 }
 
 nextButton.addEventListener('click', goToNextStep);
