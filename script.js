111da002f0b06336716dc2401e0aab4aa0d36594
Christian <christian.quinlan36@gmail.com>
2020-12-17 12:42:40 +0000

    adding further code from author

--- script.js
+++ script.js
@@ -3,41 +3,64 @@ const prevButton = document.querySelector('#prev');
 const circles = document.querySelectorAll('.circle');
 const progressBar = document.querySelector('#progress');
 
-let progressPercent = 0;
-let progressStep = 1;
+// *************************************************
+// MY VERSION
+// let progressPercent = 0;
+// let progressStep = 1;
 
-const enableDisableBtn = ()=>{
+// const enableDisableBtn = ()=>{
   
-  if(progressStep ===1 && progressStep < 4){
-    nextButton.disabled = false;
-    prevButton.disabled = true;
-  }else if(progressStep >1 && progressStep < 4){
-    nextButton.disabled = false;
-    prevButton.disabled = false;
-  }else{
-    nextButton.disabled= true;
+//   if(progressStep ===1 && progressStep < 4){
+//     nextButton.disabled = false;
+//     prevButton.disabled = true;
+//   }else if(progressStep >1 && progressStep < 4){
+//     nextButton.disabled = false;
+//     prevButton.disabled = false;
+//   }else{
+//     nextButton.disabled= true;
+//   }
+// }
+
+// const goToNextStep = ()=>{
+
+//     circles[progressStep].classList.add('active');
+//     progressPercent += 33;
+//     progressBar.style.width = `${progressPercent}%`;
+//     progressStep++;
+//     enableDisableBtn();
+//     console.log(progressStep);
+// }
+
+// const goToPrevStep = ()=>{
+//   progressPercent -=33;
+//   progressStep--;
+//   circles[progressStep].classList.remove('active');
+//   progressBar.style.width = `${progressPercent}%`;
+  
+//   enableDisableBtn();
+//   console.log(progressStep);
+// }
+
+// nextButton.addEventListener('click', goToNextStep);
+// prevButton.addEventListener('click', goToPrevStep);
+
+// ***************************************************
+// AUTHORS VERSION
+
+let currentActive = 1;
+
+nextButton.addEventListener('click', ()=>{
+  currentActive++
+
+  if(currentActive > circles.length){
+    currentActive = circles.length;
   }
-}
+});
 
-const goToNextStep = ()=>{
+prevButton.addEventListener('click', ()=>{
+  currentActive--
 
-    circles[progressStep].classList.add('active');
-    progressPercent += 33;
-    progressBar.style.width = `${progressPercent}%`;
-    progressStep++;
-    enableDisableBtn();
-    console.log(progressStep);
-}
-
-const goToPrevStep = ()=>{
-  progressPercent -=33;
-  progressStep--;
-  circles[progressStep].classList.remove('active');
-  progressBar.style.width = `${progressPercent}%`;
-  
-  enableDisableBtn();
-  console.log(progressStep);
-}
-
-nextButton.addEventListener('click', goToNextStep);
-prevButton.addEventListener('click', goToPrevStep);
+  if(currentActive < 1){
+    currentActive = 1;
+  }
+});
